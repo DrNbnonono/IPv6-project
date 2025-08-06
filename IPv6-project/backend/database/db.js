1e5eafa31f4ade4a8f2bc86de2015f790503b435
星星沅圆 <161320616+Elomeme@users.noreply.github.com>
2025-08-06 19:24:59 +0800

    Update db.js

--- IPv6-project/backend/database/db.js
+++ IPv6-project/backend/database/db.js
@@ -7,6 +7,7 @@ dotenv.config();
 // 数据库配置
 const dbConfig = {
   host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || 'linux_db',
   password: process.env.DB_PASSWORD || 'root',
   database: process.env.DB_NAME || 'ipv6_project',
